c1fc6be90f8ab95ed88f8bb34907f53f746b62dc
gunr2171 <kevin@kevin-hunter.net>
2015-01-06 22:36:48 -0500

    user command class

---
+++ CVChatbot/CVChatbot/Commands/UserCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVChatbot.Commands
+{
+    /// <summary>
+    /// Represents a commands that a user types into chat to control the chat bot.
+    /// </summary>
+    public abstract class UserCommand
+    {
+
+    }
+}
