e3b7ce65b19f674f17f452e736113f2f69759abd
gunr2171 <mathwiz134@gmail.com>
2015-01-08 09:35:33 -0500

    logic for figuring out if command, need to get triggers working as well

--- CVChatbot/CVChatbot/RoomManager.cs
+++ CVChatbot/CVChatbot/RoomManager.cs
@@ -18,7 +18,8 @@ namespace CVChatbot
         public RoomManager(string username, string email, string password)
         {
             chatClient = new Client(username, email, password);
-            cvChatRoom = chatClient.JoinRoom("http://chat.stackoverflow.com/rooms/1/sandbox");
+            cvChatRoom = chatClient.JoinRoom("http://chat.stackoverflow.com/rooms/68414/cvchatbot-testing");
+            cvChatRoom.StripMentionFromMessages = false;
 
             var startMessage = cvChatRoom.PostMessage("Hey everyone! (SO Close Vote Chatbot started)");
 
@@ -32,6 +33,20 @@ namespace CVChatbot
 
         private async void cvChatRoom_NewMessage(Message newMessage)
         {
+            //first, check if the incoming message is addressed to the chat user
+
+            if (newMessage.ParentID <= 0) //this is not a reply
+                return;
+
+            var repliedMessage = cvChatRoom.GetMessage(newMessage.ParentID);
+
+            if (repliedMessage.AuthorID != cvChatRoom.Me.ID)
+                return;
+
+            //the message is directed to the chat user, process it
+            //figure out if the message is a command
+
+
             if (newMessage.Content == "repeat")
             {
                 await Task.Run(() =>
