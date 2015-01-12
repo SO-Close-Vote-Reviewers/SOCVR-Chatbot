9e593178a28af9e51948f3c371608e1ba9c91df5
gunr2171 <mathwiz134@gmail.com>
2015-01-12 10:45:43 -0500

    structure for new "last session" commands

---
+++ CVChatbot/CVChatbot/Commands/LastSessionEditCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVChatbot.Commands
+{
+    /// <summary>
+    /// Used for editing the last session's review count.
+    /// </summary>
+    public class LastSessionEditCount : UserCommand
+    {
+        public override bool DoesInputTriggerCommand(ChatExchangeDotNet.Message userMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RunCommand(ChatExchangeDotNet.Message userMessage, ChatExchangeDotNet.Room chatRoom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CVChatbot/CVChatbot/Commands/LastSessionStats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVChatbot.Commands
+{
+    /// <summary>
+    /// Shows stats about the last session
+    /// </summary>
+    public class LastSessionStats : UserCommand
+    {
+        public override bool DoesInputTriggerCommand(ChatExchangeDotNet.Message userMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RunCommand(ChatExchangeDotNet.Message userMessage, ChatExchangeDotNet.Room chatRoom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
