849984f14daa0cb950adb2bb32d267db3ab705a6
gunr2171 <mathwiz134@gmail.com>
2015-01-16 12:28:03 -0500

    making the (empty) RunningCommands file. Going to need some restructuring on the abstract methods in UserCommand before I continue.

---
+++ CVChatbot/CVChatbot/Commands/RunningCommands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVChatbot.Commands
+{
+    public class RunningCommands : UserCommand
+    {
+
+        public override bool DoesInputTriggerCommand(ChatExchangeDotNet.Message userMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RunCommand(ChatExchangeDotNet.Message userMessage, ChatExchangeDotNet.Room chatRoom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetHelpText()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ActionPermissionLevel GetPermissionLevel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
