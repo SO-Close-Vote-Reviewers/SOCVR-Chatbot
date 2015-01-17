853866318907f2fe8f131341ff171fbdb23b0037
gunr2171 <kevin@kevin-hunter.net>
2015-01-17 11:25:57 -0500

    starting idea for ChatbotActionRegister
    starting for #18

---
+++ CVChatbot/CVChatbot/Actions/ChatbotActionRegister.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVChatbot.Actions
+{
+    public static class ChatbotActionRegister
+    {
+        public static List<ChatbotAction> ChatActions
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
