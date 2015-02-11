af6d612dde0a245cfc7addadfef2e2ea359a542e
gunr2171 <mathwiz134@gmail.com>
2015-02-11 09:01:00 -0500

    start of the DatabaseAccessor

---
+++ CVChatbot/CVChatbot.Bot/Database/DatabaseAccessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheCommonLibrary.Sql;
+using System.Data;
+
+namespace CVChatbot.Bot.Database
+{
+    class DatabaseAccessor : SqlScriptAccessorBase
+    {
+        public DatabaseAccessor(string connectionString) : base(connectionString) { }
+
+        public List<object> GetCompletedTags(int personThreshold, int maxReturnEntries)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
