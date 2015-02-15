5c728e85c42f5aac8f52d69ad595c621e93d0bbb
gunr2171 <kevin@kevin-hunter.net>
2015-02-15 11:02:55 -0500

    Adds the My Completed Tags command fixes #52.

---
+++ CVChatbot/CVChatbot.Bot/Database/UserCompletedTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVChatbot.Bot.Database
+{
+    public class UserCompletedTag
+    {
+        public string TagName { get; set; }
+        public int TimesCleared { get; set; }
+        public DateTimeOffset LastTimeCleared { get; set; }
+    }
+}
