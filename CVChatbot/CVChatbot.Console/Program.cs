1a02733ae8ffae1538de84c4fc0f9cdd7cc027c1
rschrieken <rene.schrieken@gmail.com>
2015-03-26 23:05:39 +0100

    this runs and works and has changes in both the CE.NET as the ChatBot and console projects

--- CVChatbot/CVChatbot.Console/Program.cs
+++ CVChatbot/CVChatbot.Console/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TCL.Extensions;
 
@@ -12,39 +13,44 @@ namespace CVChatbot.Console
     {
         static RoomManager mng;
         static bool shutdownOrderGiven = false;
+        // waithandle for shutdown
+        static ManualResetEvent shutdownEvent = new ManualResetEvent(false);
 
         static void Main(string[] args)
         {
             WriteToConsole("Starting program");
 
-            mng = new RoomManager();
-            mng.ShutdownOrderGiven += mng_ShutdownOrderGiven;
-            mng.InformationMessageBroadcasted += mng_InformationMessageBroadcasted;
-
-            WriteToConsole("Gathering settings");
-
-            var settings = new InstallationSettings()
+            // dispise our RoomManager
+            using (mng = new RoomManager())
             {
-                ChatRoomUrl = SettingsFileAccessor.GetSettingValue<string>("ChatRoomUrl"),
-                Email = SettingsFileAccessor.GetSettingValue<string>("LoginEmail"),
-                Password = SettingsFileAccessor.GetSettingValue<string>("LoginPassword"),
-                StartUpMessage = SettingsFileAccessor.GetSettingValue<string>("StartUpMessage"),
-                StopMessage = SettingsFileAccessor.GetSettingValue<string>("StopMessage"),
-                MaxReviewLengthHours = SettingsFileAccessor.GetSettingValue<int>("MaxReviewLengthHours"),
-                DefaultCompletedTagsPeopleThreshold = SettingsFileAccessor.GetSettingValue<int>("DefaultCompletedTagsPeopleThreshold"),
-                MaxTagsToFetch = SettingsFileAccessor.GetSettingValue<int>("MaxFetchTags"),
-                DatabaseConnectionString = SettingsFileAccessor.GetSettingValue<string>("DatabaseConnectionString"),
-                PingReviewersDaysBackThreshold = SettingsFileAccessor.GetSettingValue<int>("PingReviewersDaysBackThreshold"),
-                DefaultNextTagCount = SettingsFileAccessor.GetSettingValue<int>("DefaultNextTagCount"),
-            };
+                mng.ShutdownOrderGiven += mng_ShutdownOrderGiven;
+                mng.InformationMessageBroadcasted += mng_InformationMessageBroadcasted;
 
-            WriteToConsole("Joining room");
-            mng.JoinRoom(settings);
+                WriteToConsole("Gathering settings");
 
-            WriteToConsole("Running wait loop");
-            while (!shutdownOrderGiven)
-            {
-                //thumbs be moving
+                var settings = new InstallationSettings()
+                {
+                    ChatRoomUrl = SettingsFileAccessor.GetSettingValue<string>("ChatRoomUrl"),
+                    Email = SettingsFileAccessor.GetSettingValue<string>("LoginEmail"),
+                    Password = SettingsFileAccessor.GetSettingValue<string>("LoginPassword"),
+                    StartUpMessage = SettingsFileAccessor.GetSettingValue<string>("StartUpMessage"),
+                    StopMessage = SettingsFileAccessor.GetSettingValue<string>("StopMessage"),
+                    MaxReviewLengthHours = SettingsFileAccessor.GetSettingValue<int>("MaxReviewLengthHours"),
+                    DefaultCompletedTagsPeopleThreshold = SettingsFileAccessor.GetSettingValue<int>("DefaultCompletedTagsPeopleThreshold"),
+                    MaxTagsToFetch = SettingsFileAccessor.GetSettingValue<int>("MaxFetchTags"),
+                    DatabaseConnectionString = SettingsFileAccessor.GetSettingValue<string>("DatabaseConnectionString"),
+                    PingReviewersDaysBackThreshold = SettingsFileAccessor.GetSettingValue<int>("PingReviewersDaysBackThreshold"),
+                    DefaultNextTagCount = SettingsFileAccessor.GetSettingValue<int>("DefaultNextTagCount"),
+                };
+
+                WriteToConsole("Joining room");
+                mng.JoinRoom(settings);
+
+                WriteToConsole("Running wait loop");
+
+                // wait to get signaled
+                // we do it this way because this is cross-thread
+                shutdownEvent.WaitOne();
             }
         }
 
@@ -56,7 +62,8 @@ namespace CVChatbot.Console
         static void mng_ShutdownOrderGiven(object sender, EventArgs e)
         {
             WriteToConsole("Shutdown order given.");
-            shutdownOrderGiven = true;
+            // signal threads that wait for this
+            shutdownEvent.Set();
         }
 
         private static object writeToConsoleLockObject = new object();
