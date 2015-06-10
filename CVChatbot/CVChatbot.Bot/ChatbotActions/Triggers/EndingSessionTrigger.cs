6dc71c927f8d4d9a2742f25274dca7eb8aa5f582
Sam <ArcticEcho@users.noreply.github.com>
2015-06-10 21:15:27 +0100

    Make session ending trigger redundant
    And apparently the .csproj file wasn't updated in the last commit.

--- CVChatbot/CVChatbot.Bot/ChatbotActions/Triggers/EndingSessionTrigger.cs
+++ CVChatbot/CVChatbot.Bot/ChatbotActions/Triggers/EndingSessionTrigger.cs
@@ -1,8 +1,4 @@
 ﻿using ChatExchangeDotNet;
-using CVChatbot.Bot.ChatbotActions.Commands;
-using CVChatbot.Bot.Database;
-using System;
-using TCL.Extensions;
 
 namespace CVChatbot.Bot.ChatbotActions.Triggers
 {
@@ -17,38 +13,8 @@ namespace CVChatbot.Bot.ChatbotActions.Triggers
         /// <returns></returns>
         protected bool EndSession(Message userMessage, Room chatRoom, int? itemsReviewed, InstallationSettings settings)
         {
-            var da = new DatabaseAccessor(settings.DatabaseConnectionString);
-
-            // Find the latest session by that user.
-            var latestSession = da.GetLatestOpenSessionForUser(userMessage.AuthorID);
-
-            // First, check if there is a session.
-            if (latestSession == null)
-            {
-                chatRoom.PostReplyOrThrow(userMessage, "I don't seem to have the start of your review session on record. I might have not been running when you started, or some error happened.");
-                return false;
-            }
-
-            // Check if session is greater than [MAX_REVIEW_TIME].
-            var maxReviewTimeHours = settings.MaxReviewLengthHours;
-
-            var timeThreshold = DateTimeOffset.Now.AddHours(-maxReviewTimeHours);
-
-            if (latestSession.SessionStart < timeThreshold)
-            {
-                var timeDelta = DateTimeOffset.Now - latestSession.SessionStart;
-
-                var message = "Your last uncompleted review session was {0} ago. Because it has exceeded my threshold ({1} hours), I can't mark that session with this information. "
-                    .FormatInline(timeDelta.ToUserFriendlyString(), maxReviewTimeHours) +
-                    "Use the command '{0}' to forcefully end that session."
-                    .FormatInline(ChatbotActionRegister.GetChatBotActionUsage<EndSession>());
-
-                chatRoom.PostReplyOrThrow(userMessage, message);
-                return false;
-            }
-
-            // It's all good, mark the info as done.
-            da.EndReviewSession(latestSession.Id, itemsReviewed);
+            var message = "You don't need to post this message anymore! When you finish reviewing I should notice it and record the end of the record.";
+            chatRoom.PostReplyOrThrow(userMessage, message);
             return true;
         }
     }
