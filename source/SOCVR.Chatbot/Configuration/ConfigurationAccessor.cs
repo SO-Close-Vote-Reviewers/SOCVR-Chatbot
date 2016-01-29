6418c7a95ecbb3c0f0e1795ebb215bb38474aa75
gunr2171 <kevin@kevin-hunter.net>
2016-01-28 19:52:45 -0500

    adding logic for GetConfigurationOption

--- source/SOCVR.Chatbot/Configuration/ConfigurationAccessor.cs
+++ source/SOCVR.Chatbot/Configuration/ConfigurationAccessor.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TCL.Extensions;
 
 namespace SOCVR.Chatbot.Configuration
 {
@@ -33,7 +36,31 @@ namespace SOCVR.Chatbot.Configuration
         /// <returns></returns>
         private static T GetConfigurationOption<T>(string key)
         {
-            throw new NotImplementedException();
+            //first, check enviornment variables
+            var envValue = Environment.GetEnvironmentVariable(key);
+
+            if (!envValue.IsNullOrWhiteSpace())
+            {
+                return envValue.Parse<T>();
+            }
+
+            //now check settings file
+            var settingsFilePath = "settings.json";
+
+            if (File.Exists(settingsFilePath))
+            {
+                var settings = JObject.Parse(File.ReadAllText(settingsFilePath));
+
+                var requestedSettingNode = settings[key];
+
+                if (requestedSettingNode != null)
+                {
+                    return requestedSettingNode.Value<T>();
+                }
+            }
+
+            //in neither location, error out
+            throw new Exception($"Unable to locate setting '{key}'.");
         }
     }
 }
