0409ff9d607e0087e50e7863d7002d19fbd7082f
gunr2171 <kevin@kevin-hunter.net>
2016-01-31 14:13:02 -0500

    filling it out

--- source/SOCVR.Chatbot/ChatbotActions/Commands/Admin/AddReview.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Admin/AddReview.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ChatExchangeDotNet;
+using TCL.Extensions;
 
 namespace SOCVR.Chatbot.ChatbotActions.Commands.Admin
 {
@@ -22,7 +23,30 @@ namespace SOCVR.Chatbot.ChatbotActions.Commands.Admin
 
         public override void RunAction(Message incomingChatMessage, Room chatRoom)
         {
-            throw new NotImplementedException();
+            using (var db = new DatabaseContext())
+            {
+                var reviewItemId = GetRegexMatchingObject()
+                    .Match(incomingChatMessage.Content)
+                    .Groups[1]
+                    .Value
+                    .Parse<int>();
+
+                var reviewerId = GetRegexMatchingObject()
+                    .Match(incomingChatMessage.Content)
+                    .Groups[2]
+                    .Value
+                    .Parse<int>();
+
+                //look up reviewer
+#warning make a method to "get user or new"
+                var targetUser = db.Users.SingleOrDefault(x => x.ProfileId == incomingChatMessage.Author.ID);
+
+                var newReview = null; //this will use the method from the user tracking
+                //http://chat.stackoverflow.com/transcript/message/28474023#28474023
+
+                db.ReviewedItems.Add(newReview);
+                db.SaveChanges();
+            }
         }
     }
 }
