b8ee2d3dc561439b27f94a6911893435e01be1a5
gunr2171 <kevin@kevin-hunter.net>
2016-01-31 12:57:44 -0500

    starting AddReview command

---
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Admin/AddReview.cs
@@ -0,0 +1,28 @@
+﻿using SOCVR.Chatbot.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatExchangeDotNet;
+
+namespace SOCVR.Chatbot.ChatbotActions.Commands.Admin
+{
+    internal class AddReview : UserCommand
+    {
+        public override string ActionDescription => "Manually adds a review to a user. Should only be used for testing.";
+
+        public override string ActionName => "Add Review";
+
+        public override string ActionUsage => "add review [review id] [user id]";
+
+        public override PermissionGroup? RequiredPermissionGroup => PermissionGroup.BotOwner;
+
+        protected override string RegexMatchingPattern => @"^add review (\d+) (\d+)$";
+
+        public override void RunAction(Message incomingChatMessage, Room chatRoom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
