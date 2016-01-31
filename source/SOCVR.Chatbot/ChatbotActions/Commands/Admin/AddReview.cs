a6f56c16f7a8fedab86dc0c3dd440874641646af
gunr2171 <kevin@kevin-hunter.net>
2016-01-31 14:59:36 -0500

    finishing up, just need to test

--- source/SOCVR.Chatbot/ChatbotActions/Commands/Admin/AddReview.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Admin/AddReview.cs
@@ -41,11 +41,10 @@ namespace SOCVR.Chatbot.ChatbotActions.Commands.Admin
                 db.EnsureUserExists(reviewerId);
                 var targetUser = db.Users.Single(x => x.ProfileId == reviewerId);
 
-                //var newReview = null; //this will use the method from the user tracking
-                //http://chat.stackoverflow.com/transcript/message/28474023#28474023
+                var parsedReviewItem = UsersWatcher.GetUserReviewedItem(reviewItemId, reviewerId);
 
-                //db.ReviewedItems.Add(newReview);
-                //db.SaveChanges();
+                db.ReviewedItems.Add(parsedReviewItem);
+                db.SaveChanges();
             }
         }
     }
