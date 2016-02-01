ca235d7f0370f9a9bb5b7f9da7f381996db3d20d
gunr2171 <kevin@kevin-hunter.net>
2016-02-01 17:55:01 -0500

    adding meta data for commands

--- source/SOCVR.Chatbot/ChatbotActions/Commands/Tracking/OptIn.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Tracking/OptIn.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChatExchangeDotNet;
+using SOCVR.Chatbot.Database;
 
 namespace SOCVR.Chatbot.ChatbotActions.Commands.Tracking
 {
-    class OptIn
+    class OptIn : UserCommand
     {
+        public override string ActionDescription => "Tells the bot to resume tracking your close vote reviewing.";
+
+        public override string ActionName => "Opt In";
+
+        public override string ActionUsage => "opt in";
+
+        public override PermissionGroup? RequiredPermissionGroup => PermissionGroup.Reviewer;
+
+        protected override string RegexMatchingPattern => "^opt in$";
+
+        public override void RunAction(Message incomingChatMessage, Room chatRoom)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- source/SOCVR.Chatbot/ChatbotActions/Commands/Tracking/OptOut.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Tracking/OptOut.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChatExchangeDotNet;
+using SOCVR.Chatbot.Database;
 
 namespace SOCVR.Chatbot.ChatbotActions.Commands.Tracking
 {
-    class OptOut
+    class OptOut : UserCommand
     {
+        public override string ActionDescription => "Tells the bot to stop tracking your close vote reviewing.";
+
+        public override string ActionName => "Opt Out";
+
+        public override string ActionUsage => "opt out";
+
+        public override PermissionGroup? RequiredPermissionGroup => PermissionGroup.Reviewer;
+
+        protected override string RegexMatchingPattern => "^opt out$";
+
+        public override void RunAction(Message incomingChatMessage, Room chatRoom)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
