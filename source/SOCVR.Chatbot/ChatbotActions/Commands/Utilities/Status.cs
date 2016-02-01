3b30ea6b76a4d1f5f6aa1f3f2da4030e982f67fa
Sam <ArcticEcho@users.noreply.github.com>
2016-02-01 21:49:53 +0000

    Use using

--- source/SOCVR.Chatbot/ChatbotActions/Commands/Utilities/Status.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Utilities/Status.cs
@@ -1,16 +1,12 @@
 ﻿using SOCVR.Chatbot.Database;
 using System;
 using System.IO;
-using System.Diagnostics;
-using System.Reflection;
 using LibGit2Sharp;
 
 namespace SOCVR.Chatbot.ChatbotActions.Commands.Utilities
 {
     internal class Status : UserCommand
     {
-        private readonly Repository repo;
-
         public override string ActionDescription =>
             "Tests if the chatbot is alive and shows simple info about it.";
 
@@ -22,27 +18,18 @@ namespace SOCVR.Chatbot.ChatbotActions.Commands.Utilities
 
         protected override string RegexMatchingPattern => @"^status\??$";
 
-
-        public Status()
-        {
-            var repoDir = SearchForRepo();
-            repo = new Repository(repoDir);
-        }
-
-        ~Status()
-        {
-            repo?.Dispose();
-        }
-
-
         public override void RunAction(ChatExchangeDotNet.Message incomingChatMessage, ChatExchangeDotNet.Room chatRoom)
         {
-            var elapsedTime = DateTime.Now - ChatBotStats.LoginDate;
-            var sha = repo.Head.Tip.Sha.Substring(0, 8);
-            var branch = repo.Head.FriendlyName;
-            var message = $"SOCVR Chatbot {branch} at `{sha}`, running for {elapsedTime.ToUserFriendlyString()}.";
+            var repoDir = SearchForRepo();
+            using (var repo = new Repository(repoDir))
+            {
+                var elapsedTime = DateTime.Now - ChatBotStats.LoginDate;
+                var sha = repo.Head.Tip.Sha.Substring(0, 8);
+                var branch = repo.Head.FriendlyName;
+                var message = $"SOCVR Chatbot {branch} at `{sha}`, running for {elapsedTime.ToUserFriendlyString()}.";
 
-            chatRoom.PostMessageOrThrow(message);
+                chatRoom.PostMessageOrThrow(message);
+            }
         }
 
         private string SearchForRepo(string curPath = ".")
