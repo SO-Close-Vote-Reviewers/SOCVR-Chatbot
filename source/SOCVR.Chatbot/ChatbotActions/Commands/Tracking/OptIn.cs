41744fd6cf16bda193b109e0f591f6fff04a378e
gunr2171 <kevin@kevin-hunter.net>
2016-02-01 18:16:50 -0500

    done with first pass at opt-in command will refactor this to a base class and work on opt-out because they are 90% the same

--- source/SOCVR.Chatbot/ChatbotActions/Commands/Tracking/OptIn.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Tracking/OptIn.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ChatExchangeDotNet;
 using SOCVR.Chatbot.Database;
+using Microsoft.Data.Entity;
 
 namespace SOCVR.Chatbot.ChatbotActions.Commands.Tracking
 {
@@ -18,11 +19,35 @@ namespace SOCVR.Chatbot.ChatbotActions.Commands.Tracking
 
         public override PermissionGroup? RequiredPermissionGroup => PermissionGroup.Reviewer;
 
-        protected override string RegexMatchingPattern => "^opt in$";
+        protected override string RegexMatchingPattern => "^opt[ -]in$";
 
         public override void RunAction(Message incomingChatMessage, Room chatRoom)
         {
-            throw new NotImplementedException();
+            using (var db = new DatabaseContext())
+            {
+                var user = db.Users
+                    .Include(x => x.Permissions)
+                    .Single(x => x.ProfileId == incomingChatMessage.Author.ID);
+
+                if (user.OptInToReviewTracking == true)
+                {
+                    //user is already opted-in
+
+                    //if you are in the reviews group your LastTrackingPreferenceChange must be set
+                    var deltaTime = DateTimeOffset.UtcNow - user.LastTrackingPreferenceChange.Value;
+
+                    var replyMessage = $"You are already opted-in to tracking, and have been in this state for {deltaTime.ToUserFriendlyString()}.";
+                    chatRoom.PostReplyOrThrow(incomingChatMessage, replyMessage);
+                    return;
+                }
+
+                //flip the setting and update the LastTrackingPreferenceChange value
+                user.OptInToReviewTracking = false;
+                user.LastTrackingPreferenceChange = DateTimeOffset.UtcNow;
+                db.SaveChanges();
+
+                chatRoom.PostReplyOrThrow(incomingChatMessage, "You have been opted-in to tracking, and will remain this way until you run `opt out`.");
+            }
         }
     }
 }
