072827f52b63e75a95e811b4ea4b2759998c0b96
Sam <ArcticEcho@users.noreply.github.com>
2016-02-01 11:45:55 +0000

    Use git info for status command

--- source/SOCVR.Chatbot/ChatbotActions/Commands/Utilities/Status.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Utilities/Status.cs
@@ -2,11 +2,14 @@
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using LibGit2Sharp;
 
 namespace SOCVR.Chatbot.ChatbotActions.Commands.Utilities
 {
     internal class Status : UserCommand
     {
+        private readonly Repository repo;
+
         public override string ActionDescription =>
             "Tests if the chatbot is alive and shows simple info about it.";
 
@@ -16,16 +19,27 @@ namespace SOCVR.Chatbot.ChatbotActions.Commands.Utilities
 
         public override PermissionGroup? RequiredPermissionGroup => null;
 
-        protected override string RegexMatchingPattern => @"^((program|chatbot|bot|what'?s your) )?status(\?)?$";
+        protected override string RegexMatchingPattern => @"^status\??$";
+
+
+        public Status()
+        {
+            var repoDir = "";
+            repo = new Repository(repoDir);
+        }
+
+        ~Status()
+        {
+            repo?.Dispose();
+        }
+
 
         public override void RunAction(ChatExchangeDotNet.Message incomingChatMessage, ChatExchangeDotNet.Room chatRoom)
         {
-#warning fix this to include the git commit (first 8 chars)
             var elapsedTime = DateTime.Now - ChatBotStats.LoginDate;
-            var assembly = Assembly.GetExecutingAssembly();
-            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            var version = fvi.FileVersion;
-            var message = $"SOCVR ChatBot version {version}, running for {elapsedTime.ToUserFriendlyString()}.";
+            var sha = repo.Head.Tip.Sha.Substring(0, 8);
+            var branch = repo.Head.FriendlyName;
+            var message = $"SOCVR Chatbot {branch} at `{sha}`, running for {elapsedTime.ToUserFriendlyString()}.";
 
             chatRoom.PostMessageOrThrow(message);
         }
