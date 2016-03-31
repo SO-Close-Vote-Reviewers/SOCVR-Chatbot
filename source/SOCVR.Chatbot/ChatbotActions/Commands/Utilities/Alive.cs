2c06852925f4bee256c6417eea822990a306c6b9
gunr2171 <kevin@kevin-hunter.net>
2016-03-31 19:17:14 -0400

    alive modifications

--- source/SOCVR.Chatbot/ChatbotActions/Commands/Utilities/Alive.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Utilities/Alive.cs
@@ -16,19 +16,19 @@ namespace SOCVR.Chatbot.ChatbotActions.Commands.Utilities
 
         public override bool UserMustBeInAnyPermissionGroupToRun => false;
 
-        protected override string RegexMatchingPattern => @"^(?:(?:are )?you )?(alive|still there|(still )?with us)\??$";
+        protected override string RegexMatchingPattern => @"^alive$";
 
         public override void RunAction(ChatExchangeDotNet.Message incomingChatMessage, ChatExchangeDotNet.Room chatRoom)
         {
             var responsePhrases = new List<string>()
             {
-                "I'm alive and kicking!",
-                "Still here you guys!",
-                "I'm not dead yet!",
-                "I feel... happy!",
-                "I think I'll go for a walk...",
-                "I don't want to go on the cart!",
-                "I feel fine.",
+                "Nope",
+                "What do you think?",
+                "Well I responded, that good enough for you?",
+                "I was just on my way out.",
+                "Oh, you _actually_ care? Sure you do...",
+                "...",
+                "'alive'? You can't even be bothered to make a whole sentence. What am I to you, some sort of bot?"
             };
 
             var phrase = responsePhrases.PickRandom();
