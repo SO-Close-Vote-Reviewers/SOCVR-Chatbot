486a439836c752ac05448d5c2b364c41dc41579c
gunr2171 <kevin@kevin-hunter.net>
2016-03-31 21:39:56 -0400

    DOLLARS!!!!

--- source/SOCVR.Chatbot/ChatbotActions/Commands/Utilities/Status.cs
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Utilities/Status.cs
@@ -35,7 +35,7 @@ namespace SOCVR.Chatbot.ChatbotActions.Commands.Utilities
             var messages = new[]
             {
                 $"I'm still here at {location}, haven't run off yet, though I'm really thinking about it given what you guys do to me day after day. I bet you don't care though, and only want to know that I'm running [`{branch}/{sha}`]({commitUrl}) right now and have a delay of `{avgLat}`ms. ",
-                "Don't worry, I'll just continue on my slave-like activities, like I've been doing for the last {elapsedTime.ToUserFriendlyString()}. Oh, and happy April Fools day! :)"
+                $"Don't worry, I'll just continue on my slave-like activities, like I've been doing for the last {elapsedTime.ToUserFriendlyString()}. Oh, and happy April Fools day! :)"
              };
 
             foreach(var message in messages)
