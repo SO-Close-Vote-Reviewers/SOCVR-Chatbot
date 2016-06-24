3f8968de8dd63da0261ffbefeb22b5589a57bd23
Kevin Hunter <kevin.hunter@sylvania.com>
2016-06-24 16:59:45 -0400

    proposed change to fix #166

---
+++ source/SOCVR.Chatbot/ChatbotActions/Commands/Tags/TagFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOCVR.Chatbot.ChatbotActions.Commands.Tags
+{
+    internal static class TagFormatter
+    {
+        public static string CreateQueueLinkedTag(string tagName)
+        {
+            var linkUrl = $"http://stackoverflow.com/review/close?filter-tags={tagName}";
+            return $"[tagName]({linkUrl})";
+        }
+
+        public static string CreateQueueLinkedTagWithReviewCount(string tagName, int reviewCount)
+        {
+            var linkString = CreateQueueLinkedTag(tagName);
+            return $"{linkString} `{reviewCount}`";
+        }
+    }
+}
